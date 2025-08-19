9b21c9c59ea6e67d2abb647c04545fab18a08985
rakeshkumar1994 <rk0737800@gmail.com>
2025-08-19 21:45:37 +1000

    feat: initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth.routes');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./src/docs/swagger.yaml');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+module.exports = app;
